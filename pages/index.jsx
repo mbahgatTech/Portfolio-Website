25952a76ce6c5a8dcac1398f6a0027b29e7e5ceb
mbahgatTech <mazenbahgat@outlook.com>
2022-07-22 22:14:25 -0400

    clean repo boiler plate

---
+++ pages/index.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mazen Bahgat Portfolio</title>
+        <meta name="Mazen Bahgat's Development Portfolio" content="Software Development Skills and Experiences" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        
+      </div>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Home;
