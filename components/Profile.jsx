915de2f701828b63c0fd7a66f6a12bcc1addf124
mbahgatTech <mazenbahgat@outlook.com>
2022-07-24 21:32:42 -0400

    created arrows.svg

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,17 +4,27 @@ import { Fragment } from 'react';
 
 const Profile = () => {
     return (
-        <div>
-            <Image 
-                src='/images/profile.jpg'
-                alt='Mazen Bahgat'
-                width={144}
-                height={144}
-                className={styles.profile}
-            /> 
-            <h1>Mazen Bahgat</h1>
-            <Fragment>Full Stack Mobile and Web Application Developer</Fragment>
-        </div>
+        <>
+            <div>
+                <Image 
+                    src='/images/profile.jpg'
+                    alt='Mazen Bahgat'
+                    width={144}
+                    height={144}
+                    className={styles.profile}
+                /> 
+                <h1>Mazen Bahgat</h1>
+                <Fragment>Full Stack Mobile and Web Application Developer</Fragment>
+            </div>
+            <div className={styles.learn}>
+                <p className={styles.learnTex}>Learn More About What I Do</p>
+                <Image
+                    src='/images/arrows.svg'
+                    height={16}
+                    width={15}
+                />
+            </div>
+        </>
     );
 }
 
