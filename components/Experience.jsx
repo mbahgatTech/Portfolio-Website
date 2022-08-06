d8f0c71d364f2f9e3c607f5f3d695fa572b15ea8
mbahgatTech <mazenbahgat@outlook.com>
2022-08-05 23:33:02 -0400

    resolved next.js build errors

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -8,10 +8,13 @@ const Experience = () => {
                 <div className='w-5/6 mx-auto'>
                     <h1 className='text-4xl tracking-tight font-bold text-blue-100 w-full text-center mx-auto mt-16'>My Work Experience</h1>
                     {experiences.map(experience => 
-                        <div className='max-w-full my-16 mx-auto bg-white rounded-xl shadow-md overflow-hidden justify-center items-center'>
+                        <div key={JSON.stringify(experience)} className='max-w-full my-16 mx-auto bg-white rounded-xl shadow-md overflow-hidden justify-center items-center'>
                             <div className='md:flex'>
                                 <div className='md:shrink-0'>
-                                    <img className='w-full h-full md:w-48' src={experience.image} alt={experience.alt} />
+                                    <picture>
+                                        <source srcSet={experience.image} type="image/webp" />
+                                        <img className='w-full h-full md:w-48' src={experience.image} alt={experience.alt} />
+                                    </picture>
                                 </div>
                                 <div className='p-8'>
                                     <div className='uppercase tracking-wide text-sm text-purple-600 font-semibold'>{experience.company}</div>
@@ -25,9 +28,12 @@ const Experience = () => {
                                 <h1 className='block mt-1 w-full text-center text-2xl leading-tight font-medium text-black'>Tech Stack</h1>
                                 <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 justify-center'>     
                                     {experience.techStack.map(elem =>
-                                        <div className="max-w-sm rounded overflow-hidden shadow-lg mb-4 mx-auto w-full">
+                                        <div key={JSON.stringify(experience)} className="max-w-sm rounded overflow-hidden shadow-lg mb-4 mx-auto w-full">
                                             <a href={elem.link}>
-                                                <img className="w-full h-60" src={elem.source} alt={elem.alt} />
+                                                <picture>
+                                                    <source srcSet={elem.source} type="image/webp" />
+                                                    <img className="w-full h-60" src={elem.source} alt={elem.alt} />
+                                                </picture>
                                                 <div className="px-6 py-4">
                                                     <div className="font-bold text-slate-500 mt-2 text-center text-xl mb-2">{elem.name}</div>
                                                 </div>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -3,19 +3,25 @@ const Socials = () => {
         <>
             <div className='mr-4'>
                 <a href='https://github.com/mbahgatTech'>
-                    <img 
-                        src='/images/github.svg'
-                        alt='Github Logo'
-                    />
+                    <picture>
+                        <source srcSet='/images/linkedin.svg' type="image/webp" />
+                        <img 
+                            src='/images/github.svg'
+                            alt='Github Logo'
+                        />
+                    </picture>
                 </a>
             </div>
 
             <div className='ml-4'>
                 <a href='https://www.linkedin.com/in/mazen-bahgat/'>
-                    <img 
-                        src='/images/linkedin.svg'
-                        alt='Linkedin Logo'
-                    />
+                    <picture>
+                        <source srcSet='/images/linkedin.svg' type="image/webp" />
+                        <img 
+                            src='/images/linkedin.svg'
+                            alt='Linkedin Logo'
+                        />
+                    </picture>
                 </a>
             </div>
         </>
