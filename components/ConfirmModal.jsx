40f4c9eef8309bcbca785599e7584dd11c6c1aed
mbahgatTech <mazenbahgat@outlook.com>
2022-08-20 17:18:34 -0400

    submit static message using email

--- components/ConfirmModal.jsx
+++ components/ConfirmModal.jsx
@@ -1,3 +1,5 @@
+import { MessageSubmit } from '../utils/Message';
+
 const Modal = ({ visible, setModal }) => {
     return (
         <div className={(!visible) ? 'hidden' : 'visible' + 'overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full justify-center items-center flex'}>
@@ -10,7 +12,10 @@ const Modal = ({ visible, setModal }) => {
                     <div className='p-6 text-center'>
                         <svg aria-hidden='true' className='mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path></svg>
                         <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>Are you sure you want to send this message?</h3>
-                        <button type='button' className='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'>
+                        <button onClick={() => {
+                            setModal(false);
+                            MessageSubmit({name: 'Mazen', email: 'emda', message: 'dsdada'});
+                        }} type='button' className='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'>
                             Yes, I'm sure
                         </button>
                         <button onClick={() => setModal(false)} type='button' className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'>No, cancel</button>
--- utils/Message.js
+++ utils/Message.js
@@ -1,11 +1,15 @@
-const MessageSubmit = (data) => {
-    axios({
-        method: 'post',
-        url: '/api/message',
-        data
-    }).catch(err => {
+import axios from 'axios';
+
+const MessageSubmit = async (data) => {    
+    try {
+        await axios({
+            method: 'post',
+            url: '/api/message',
+            data
+        });
+    } catch(err) {
         alert(err.response.data || err.message);
-    });
+    }
 }
 
 export { MessageSubmit };
