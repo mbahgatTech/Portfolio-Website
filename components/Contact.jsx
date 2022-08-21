9485fd6ccd458eed40e88330cf13cc7b7cbcf93f
mbahgatTech <mazenbahgat@outlook.com>
2022-08-21 15:21:26 -0400

    make contact inputs required

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -15,15 +15,15 @@ const Contact = ({ setModal }) => {
                 }}>
                     <div className='grid gap-6 sm:grid-cols-2'>
                         <div className='relative z-0'>
-                            <input type='text' name='name' className='peer mt-1 block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0' />
+                            <input type='text' required name='name' className='peer mt-1 block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0' />
                             <label className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500'>Name</label>
                         </div>
                         <div className='relative z-0'>
-                            <input type='text' name='email' className='peer block mt-1 w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0' />
+                            <input type='text' required name='email' className='peer block mt-1 w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0' />
                             <label className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500'>Email</label>
                         </div>
                         <div className='relative z-0 col-span-2'>
-                            <textarea name='message' rows='5' className='peer mt-1 block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0'></textarea>
+                            <textarea name='message' required rows='5' className='peer mt-1 block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-blue-600 focus:outline-none focus:ring-0'></textarea>
                             <label className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-white duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500'>Message</label>
                         </div>
                     </div>
