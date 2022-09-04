485814e8537b890e2b9d51f6b9f2f6c0830c9934
mbahgatTech <mazenbahgat@outlook.com>
2022-09-04 13:41:26 -0400

    read more button and tech stack hide on mobile

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -5,21 +5,23 @@ import { getExperienceRoutes, getData} from '../utils/Routes';
 
 const Experience = ({ data }) => {
     return (
-        <div className='bg-slate-800 min-h-screen'>
+        <div className='bg-gray-100 min-h-screen w-screen content-center items-center justify-center'>
             <Head>
                 <title>Mazen Bahgat</title>
                 <meta name="Mazen Bahgat's Development Portfolio" content="Software Development Skills and Experiences" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             
-            <div className='w-5/6 items-center justify-center h-full'>
-                <h1>{data.role}</h1>
-                <h1>{data.company}</h1>
-                <picture>
-                    <source srcSet={data.image} type="image/webp" />
-                    <img className='w-full h-full md:w-48' src={data.image} alt={`${data.company} Logo`} />
-                </picture>
-                <div dangerouslySetInnerHTML={{ __html: data.htmlContent }} />
+            <div className='items-center content-center justify-center h-full w-[60%] self-center mx-auto bg-gray-100'>
+                <div className='w-full h-full md:w-48 mx-auto bg-white aspect-square p-3 rounded-full mb-7 top-7'>
+                    <picture>
+                        <source srcSet={data.image} type="image/png" />
+                        <img className='w-full h-full md:w-48 mx-auto' src={data.image} alt={`${data.company} Logo`} />
+                    </picture>
+                </div>
+                <h1 className='text-2xl font-bold text-purple-600 sm:pr-12'>{data.company} - {data.role}</h1>
+                <h1 className='text-lg text-gray-600 mb-5 '>{data.dateRange}</h1>
+                <div className='text-gray-900' dangerouslySetInnerHTML={{ __html: data.htmlContent }} />
                 <div>
                     <Link href="/">
                         <a className='text-sky-600 hover:text-sky-400'>← Back to home</a>
--- utils/experiences/scales.md
+++ utils/experiences/scales.md
@@ -2,7 +2,7 @@
 company: Scales Nature Park
 dateRange: May 2022 - September 2022
 role: Full Stack Application Developer
-image: /images/scalesLogo.webp
+image: /images/scalesLogo.png
 ---
 
 Led sammdajfjndajfncdjadvjnd
