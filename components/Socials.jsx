5ea8c7083a02a417509363c4e11c80dbf8b102ac
mbahgatTech <81331884+mbahgatTech@users.noreply.github.com>
2022-12-24 23:56:34 -0500

    Feature: Added new download resume button and constants file

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -4,9 +4,9 @@ const Socials = ({ dark }) => {
             <div className='mr-4'>
                 <a href='https://github.com/mbahgatTech'>
                     <picture>
-                        <source srcSet={(dark) ? '/images/github-dark.svg' : '/images/github.svg'} type="image/webp" />
+                        <source srcSet={dark ? '/images/github-dark.svg' : '/images/github.svg'} type='image/webp' />
                         <img 
-                            src={(dark) ? '/images/github-dark.svg' : '/images/github.svg'}
+                            src={dark ? '/images/github-dark.svg' : '/images/github.svg'}
                             alt='Github Logo'
                         />
                     </picture>
@@ -16,9 +16,9 @@ const Socials = ({ dark }) => {
             <div className='ml-4'>
                 <a href='https://www.linkedin.com/in/mazen-bahgat/'>
                     <picture>
-                        <source srcSet={(dark) ? '/images/linkedin-dark.svg' : '/images/linkedin.svg'} type="image/webp" />
+                        <source srcSet={dark ? '/images/linkedin-dark.svg' : '/images/linkedin.svg'} type='image/webp' />
                         <img 
-                            src={(dark) ? '/images/linkedin-dark.svg' : '/images/linkedin.svg'}
+                            src={dark ? '/images/linkedin-dark.svg' : '/images/linkedin.svg'}
                             alt='Linkedin Logo'
                         />
                     </picture>
---
+++ utils/json/constants.js
@@ -0,0 +1,20 @@
+const PROFILE = {
+  MAZEN_BAHGAT: 'Mazen Bahgat',
+  JOB_TITLE: 'Full Stack Developer',
+  BRIEF: 'I am a software engineer based in Ontario, Canada. I enjoy building web, mobile and desktop applications.',
+  RESUME_BUTTON: 'Download Resume'
+};
+const NAVIGATION = [
+  { name: 'GitHub', href: 'https://github.com/mbahgatTech' },
+  { name: 'Linkedin', href: 'https://www.linkedin.com/in/mazen-bahgat/' }
+];
+const MODAL = {
+  MESSAGE_SUCCESS: 'Success: Your message has been sent!',
+  MESSAGE_FAILURE: 'Failure: We could not send your message, please try again at a later time.',
+  CLOSE_MODAL: 'Close Modal',
+  CONFIRMATION_PROMPT: 'Are you sure you want to send this message?',
+  CANCEL_OPERATION: 'No, cancel',
+  CONFIRM_OPERATION: 'Yes, I am sure'
+};
+
+export { PROFILE, NAVIGATION, MODAL };
